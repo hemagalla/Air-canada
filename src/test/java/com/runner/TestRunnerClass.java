8d503d9a0584a87f5296b2964fff84c1070319f6
hemagalla <https://github.com/hemagalla/Air-canada.git>
2021-12-08 10:05:56 +0530

    passwoed completed

--- src/test/java/com/runner/TestRunnerClass.java
+++ src/test/java/com/runner/TestRunnerClass.java
@@ -8,5 +8,12 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = {"src\\test\\resources\\facebook.feature"})
 public class TestRunnerClass { 
+	public static void user() {
+		System.out.println("User function");
+
+	}
+	public static void main(String[] args) {
+		user();
+	}
 
 }
